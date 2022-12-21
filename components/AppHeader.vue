714f24f9b0e8a2b56696d95e95b6b402e9ba83dd
abbasoffanermin <105522485+abbasoffanermin@users.noreply.github.com>
2022-12-21 17:23:38 +0400

    Add files via upload

---
+++ components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li>
+             <NuxtLink to="/" class="home">Home</NuxtLink>   
+            </li>
+            <li>
+             <NuxtLink to="/aboutpage">About</NuxtLink>   
+            </li>
+            <li>
+             <NuxtLink to="/samplepost">Sample Post</NuxtLink>   
+            </li>
+            <li>
+             <NuxtLink to="/contact">Contact</NuxtLink>   
+            </li>
+           
+            
+        </ul>
+    </div>
+</template>
+
+<style>
+ul{
+    display: flex;
+    justify-content: end;
+    margin-left: 20px;
+}
+li{
+    text-decoration: none;
+    margin-left: 20px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+app:{
+    head:{
+        title:"Yeni ders",
+        htmlAttrs:{
+        lang:'en'
+        },
+        meta:[
+            {charset:'utf-8'},
+            {name:'viewport',content:'width=device-width, initial-scale=1'},
+            {hid:'description',name:'description',content:''},
+            {name:'format-detection',content:"telephone=no"}
+        ],
+        link:[
+            {href:'../nuxt-app/assets/css/styles.css',rel:"stylesheet"}
+        ],
+        
+    }
+}
+       
+})
